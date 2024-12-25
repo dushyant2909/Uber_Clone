708ea519eaac7fa702fdc737020549aa60a29386
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-12-25 22:54:25 +0530

    Design home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 function App() {
 
   return (
-    <div className="font-extrabold bg-red-800">This is the app folder</div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/user/login" element={<UserLogin />} />
+      <Route path="/user/signup" element={<UserSignup />} />
+      <Route path="/captain/login" element={<CaptainLogin />} />
+      <Route path="/captain/signup" element={<CaptainSignup />} />
+    </Routes>)
 }
 
 export default App
